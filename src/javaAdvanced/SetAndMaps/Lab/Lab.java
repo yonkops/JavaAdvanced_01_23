72f94ceeedde79277770c19d88f7ba7ac1be951e
Yonko Petrov <yonko.petrov@gmail.com>
2023-01-22 22:30:40 +0200

    Java Advanced Exercises - Map

--- src/javaAdvanced/SetAndMaps/Lab/Lab.java
+++ src/javaAdvanced/SetAndMaps/Lab/Lab.java
@@ -1,21 +1,39 @@
 package javaAdvanced.SetAndMaps.Lab;
 
-import java.util.HashSet;
-import java.util.LinkedHashSet;
-import java.util.TreeSet;
+import java.util.*;
 
 public class Lab {
     public static void main(String[] args) {
         // Does not guarantee the constant order of elements over time
-        HashSet<String> someHashSet = new HashSet<String>();
+        HashSet<String> someHashSet = new HashSet<>();
         // The elements are ordered incrementally
-        TreeSet<String> someTreeSet = new TreeSet<String>();
+        TreeSet<String> someTreeSet = new TreeSet<>();
         // The order of appearance is preserved
-        LinkedHashSet<String> soleLinkedHashSet = new LinkedHashSet<String>();
+        LinkedHashSet<String> soleLinkedHashSet = new LinkedHashSet<>();
         // Print Set
         for (String members : someHashSet
         ) {
             System.out.println(members);
         }
+/*        for (Map.Entry<String, Map<String, List<String>>> entry : continents.entrySet()) {
+            System.out.println(entry.getKey() + ":");
+            for (Map.Entry<String, List<String>> country : entry.getValue().entrySet()) {
+                System.out.printf("  %s -> ", country.getKey());
+                System.out.print(String.join(", ", country.getValue()));
+                System.out.println();
+            }
+        }*/
+
+        /*
+         continents.entrySet().stream()
+              .forEach(entry -> {
+                  System.out.println(entry.getKey() + ":");
+                  entry.getValue().entrySet().stream()
+                          .forEach(country -> {
+                              String output = String.join(", ", country.getValue());
+                              System.out.println(" " + country.getKey() + " -> " + output);
+                          });
+              });
+        */
     }
 }
